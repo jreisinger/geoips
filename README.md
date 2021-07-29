5dad8e81f79e6aaf655df1cf2453995bd003c94a
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-07-29 13:36:42 +0200

    print sorted table

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+geoips prints geographic locations of IP addresses. The output is sorted by
+country and then by city.
+
 ```
 go build
+./geoips 1.1.1.1 8.8.8.8
 cat ips.txt | ./geoips
 ```
--- main.go
+++ main.go
@@ -6,21 +6,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"sort"
+	"text/tabwriter"
 
 	"github.com/jreisinger/checkip"
 )
 
-/*
-1. Get country and city of each IP address.
-2. Print counts per city showing also country.
-*/
-
-type Counts map[string]map[string][]net.IP
-
 func main() {
 	ips := parseIPs(getIPs())
 
-	counts := make(map[string]map[string][]net.IP)
+	var locations []Location
 
 	for _, ip := range ips {
 		g := &checkip.Geo{}
@@ -29,17 +24,46 @@ func main() {
 			log.Printf("while getting geolocation: %v", err)
 			continue
 		}
-		if _, ok := counts[g.Country]; !ok {
-			counts[g.Country] = make(map[string][]net.IP)
-		}
-		counts[g.Country][g.City] = append(counts[g.Country][g.City], ip)
+		l := Location{ip, g.Country, g.City}
+		locations = append(locations, l)
 	}
+	sort.Sort(customSort{locations, func(x, y Location) bool {
+		if x.Country != y.Country {
+			return x.Country < y.Country
+		}
+		if x.City != y.City {
+			return x.City < y.City
+		}
+		return false
+	}})
+	printLocations(locations)
+}
 
-	for country, m := range counts {
-		for city, ips := range m {
-			fmt.Printf("%s;%s;%d\n", country, city, len(ips))
-		}
+type Location struct {
+	IP      net.IP
+	Country string
+	City    string
+}
+
+type customSort struct {
+	l    []Location
+	less func(x, y Location) bool
+}
+
+func (x customSort) Len() int           { return len(x.l) }
+func (x customSort) Less(i, j int) bool { return x.less(x.l[i], x.l[j]) }
+func (x customSort) Swap(i, j int)      { x.l[i], x.l[j] = x.l[j], x.l[i] }
+
+func printLocations(locations []Location) {
+	const format = "%v\t%v\t%v\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Country", "City", "IP address")
+	fmt.Fprintf(tw, format, "-------", "----", "----------")
+	for _, l := range locations {
+		// you don't have to derefence here like (*b).title
+		fmt.Fprintf(tw, format, l.Country, l.City, l.IP)
 	}
+	tw.Flush() // calculate column widths and print table
 }
 
 func getIPs() []string {
